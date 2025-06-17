27b34a49235cda2625ab8828d450069cba408ecc
VarinKala <varinkaala04@gmail.com>
2025-06-17 10:35:18 +0530

    Jun16: 1 900s, 1 1000s, 1 1100s

---
+++ 1000/23 Different Divisors.cpp
@@ -0,0 +1,70 @@
+// 1474B
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <set>
+#include <map>
+#include <numeric>
+using namespace std;
+
+// Type definitions
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+// Macros
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define frange(i, a, b) for (ll i = (a); i < (b); ++i)
+#define frev(i, a, b) for (ll i = (a)-1; i >= (b); --i)
+#define print_vec(x) for (ll i = 0; i < (ll)x.size(); i++) cout << x[i] << " "; cout << endl
+#define pb push_back
+#define F first
+#define S second
+
+// Debug macro
+#ifdef LOCAL
+#define debug(x) cerr << #x << ": " << x << '\n'
+#else
+#define debug(x)
+#endif
+
+ll findPrime(ll n) {
+  for (ll i = n;; i++) {
+    bool prime = true;
+    for (ll j = 2; j*j <= i; j++) {
+      if (i % j == 0) {
+        prime = false;
+        break;
+      }
+    }
+    if (prime) return i;
+  }
+}
+
+void solve() {
+  ll d; cin >> d;
+
+  ll a = findPrime(d+1);
+  ll b = findPrime(d+a);
+
+  cout << min(a*a*a, a*b) << endl;
+}
+
+int main() {
+  fast_io;
+  int t;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
+
---
+++ 1100/23 Difference of GCDs.cpp
@@ -0,0 +1,66 @@
+// 1708B
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <set>
+#include <map>
+#include <numeric>
+using namespace std;
+
+// Type definitions
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+// Macros
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define frange(i, a, b) for (ll i = (a); i < (b); ++i)
+#define frev(i, a, b) for (ll i = (a)-1; i >= (b); --i)
+#define print_vec(x) for (ll i = 0; i < (ll)x.size(); i++) cout << x[i] << " "; cout << endl
+#define pb push_back
+#define F first
+#define S second
+
+// Debug macro
+#ifdef LOCAL
+#define debug(x) cerr << #x << ": " << x << '\n'
+#else
+#define debug(x)
+#endif
+
+void solve() {
+  ll n, l, r; cin >> n >> l >> r;
+
+  vll a(n);
+  a[0] = l;
+  frange(i, 2, n+1) {
+    if (l % i == 0) a[i-1] = l;
+    else if (r >= l + i - (l%i)) a[i-1] = l + i - (l%i);
+    else {
+      cout << "NO" << endl;
+      return;
+    }
+  }
+
+  cout << "YES" << endl;
+  print_vec(a);
+}
+
+int main() {
+  fast_io;
+  int t;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
+
